fba6d73b93fa1470aa658d49711a8269060fee30
Krzysztof Rudko <chris.rudko@gmail.com>
2015-02-27 21:54:11 +0100

    Test task no. 4/5/6. Sorry, short enough, that i forgot to commit...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+City.create(name: 'Vienna', short_name: 'Vn')
+City.create(name: 'Oslo', short_name: 'Os')
+City.create(name: 'Milan', short_name: 'Mn')
+City.create(name: 'Lyon', short_name: 'Ly')
+City.create(name: 'New York', short_name: 'NY')
+City.create(name: 'Rome', short_name: 'Rm')
+
